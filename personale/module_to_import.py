50ca550bb24b819d43fc6949cae2d22ce30da502
pippo <orlando.bzt@gmail.com>
2023-04-11 20:23:38 +0200

    modifiche esercizi

---
+++ personale/module_to_import.py
@@ -0,0 +1,6 @@
+def greet(x, y, Z):
+    print("HELLO,", X, Y + ",", "the", z + "!")
+
+    name = "angela"
+    second_name = "David"
+    profession = "scholar"
---
+++ personale/module_to_import_to.py
@@ -0,0 +1,5 @@
+from module_to_import import greet, name, second_name, profession
+
+greet(name, second_name, profession)
+
+
