d0de46411721868e489d7b468d6fcc3f52aaf02e
PPBC01-15 <dayafter@tiscali.it>
2023-04-13 19:41:26 +0200

    13-04

--- personale/hello.py
+++ personale/hello.py
@@ -1,2 +1,9 @@
 print("Hello World!")
 print("Questo è il mio primo modulo creato da me !")
+
+lista = ["primo","secondo","terzo"]
+n=range((len(lista)))
+print(range)
+for i in n:
+    print("elemneto: ", lista[i], [i])
+    i+=1
