4d52c3f1d83d84b2fbffaa9e6d5c635d30ae9608
PPBC01-20 <fabiobono899@gmail.com>
2023-04-20 19:40:20 +0200

    Mod

---
+++ personale/module_to_import.py
@@ -0,0 +1,9 @@
+def greet(x, y, z):
+    print("Hello,", x, y + ",", "the", z + "!")
+
+name = "Angela"
+second_name = "Davis"
+profession = "scholar"
+
+if __name__ == 'main':
+    print('Ciao a tutti!', name)
