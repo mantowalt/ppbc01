219694aba810069024b2d00b5bcd8fc9bb24b32b
PPBC01-18 <s.eljaadaoui@gmail.com>
2023-04-27 20:45:42 +0200

    my file

---
+++ personale/module_to_import.py
@@ -0,0 +1,11 @@
+def greet(x, y, z):
+    print("Hello,", x, y + ",", "the", z + "!")
+
+name = "Angela"
+second_name = "Davis"
+profession = "scholar"
+
+if __name__=="__main__":
+    print("ciao a tutti!")
+   
+
---
+++ personale/modulo_to_import_to.py
@@ -0,0 +1,6 @@
+from module_to_import import greet, name, second_name, profession
+
+print("modele1"__name__)
+
+greet(name, second_name, profession)
+
