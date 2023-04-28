39305d3bd19d278f7c749a1ee8b46b6e2f00db73
PPBC01-05 <furnarifabio@gmail.com>
2023-04-28 19:02:46 +0200

    Modifiche

---
+++ Personale/hello.py
@@ -0,0 +1,3 @@
+# hello.py script
+
+print("Hello, World!")
---
+++ Personale/module_to_import_to.py
@@ -0,0 +1,8 @@
+# module_to_import_to.py
+
+import module_to_import
+print('Modulo 1:',__name__)
+
+module_to_import.greet(module_to_import.name,
+                       module_to_import.second_name,
+                       module_to_import.profession)
